fa8ba6a1c3467b607b3388009099d6f531a7498b
AlexLeyMar <80998860+AlexLeyMar@users.noreply.github.com>
2021-10-21 01:14:27 -0500

    Examen, entrega 2

--- controllers/deck.js
+++ controllers/deck.js
@@ -1,28 +1,53 @@
+const exp = require("constants")
 const Carta = require("../models/deck")
 
 valores = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "Q", "J", "K"]
 palos = ["Corazon", "Diamante", "Trebol", "Pica"]
 colores = ["Negro", "Rojo"]
 
+function checkIfRepeated(p1, p2, p3){
+
+    let flag = 0
+
+    Carta.find({valor:String(p1), color:String(p2), palo:String(p3)})
+        .then(cartas=>{
+            console.log(cartas)
+        }, flag = 1)
+        .catch((err)=>{
+            console.log(err)
+        })
+
+    return flag
+}
+
+
 exports.postAgregarCarta = async (req, res)=>{
     const carta = new Carta(req.body);
-    if (valores.includes(carta.numero) == true && palos.includes(carta.palo) == true 
-    && colores.includes(carta.color) == true){
+
+    console.log(palos.includes(carta.palo))
+    console.log(valores.includes(carta.valor))
+    console.log(colores.includes(carta.color))
+    console.log(checkIfRepeated(req.body))
+    if (valores.includes(carta.valor) && palos.includes(carta.palo)
+    && colores.includes(carta.color) /*&& checkIfRepeated(carta.valor, carta.color, carta.palo) == 0*/){
         try{
             await carta.save()
             console.log(carta)
             console.log("Carta Registrada")
+            res.json({estado:"Aceptado"})
         }catch(err){
             console.log(err)
+            res.json({estado:"error"})
         }
     }else{
         console.log("Carta no valida")
+        res.json({estado:"Carta no valida"})
     }
 }
 
-/*
-exports.getObtenerCarta = (req, res)=>{
-    Carta.findAll()
+
+exports.getObtenerCarta = async (req, res)=>{
+    Carta.find()
         .then(cartas=>{
             console.log(cartas)
             res.json(cartas)
@@ -31,13 +56,5 @@ exports.getObtenerCarta = (req, res)=>{
             console.log(err)
             res.json({estado:"ERROR"})
         })
-}*/
-
-exports.getObtenerCarta = async (req, res)=>{
-    try{
-        console.log(Carta.find())
-        res.json(Carta.find())
-    }catch(err){
-        console.log(err)
-    }
 }
+
--- models/deck.js
+++ models/deck.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const DeckSchema = mongoose.Schema({
     //_id: mongoose.Schema.Types.ObjectId,
-    numero:{
+    valor:{
         type:String,
         required:true
     },
